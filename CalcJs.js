ba5dc2c4e616edf6fcac2298c31c4764a505a7db
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2023-12-10 20:57:56 +0300

    Update CalcJs.js

--- CalcJs.js
+++ CalcJs.js
@@ -1,9 +1,15 @@
-var buttons = document.querySelectorAll('td"]').value= value;
+// const specialChar=['+','-','=','÷','x']
+// const findChar= specialChar.find(function(element){
+
+// })
+var buttons = document.querySelectorAll('td').value;
 
 addValue.addEventListener("click",calculation)
 
 function calculation(value) {
     var currentText = document.getElementById("text").value;
     document.getElementById("text").value = currentText + value;
+    
+
 }
 
