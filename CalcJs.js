2b6cc0363ec7b7395b0fd790d06dfa6607ed04b5
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2023-12-10 20:49:49 +0300

    Update CalcJs.js

--- CalcJs.js
+++ CalcJs.js
@@ -1,60 +1,9 @@
-function calculation(values)
-{
-    var addValue = document.getElementById("add").value;
-    document.getElementById("text").value = addValue;
-    var addValue = document.getElementById("1").value;
-    document.getElementById("text").value = addValue;
-    var addValue = document.getElementById("2").value;
-    document.getElementById("text").value = addValue;
+var buttons = document.querySelectorAll('td"]').value= value;
 
+addValue.addEventListener("click",calculation)
 
-    /*switch(values) {
-        case '1':
-            document.getElementById("text").value = values;
-           
-        case '2':
-            document.getElementById("text").value = values;
-           
-        case '3':
-            document.getElementById("text").value = values;
-           
-        case '4':
-            document.getElementById("text").value = values;
-           
-        case '5':
-            document.getElementById("text").value = values;
-           
-        case '6':
-            document.getElementById("text").value = values;
-           
-        case '7':
-            document.getElementById("text").value = values;
-           
-        case '8':
-            document.getElementById("text").value = values;
-           
-        case '9':
-            document.getElementById("text").value = values;
-           
-        case '0':
-            document.getElementById("text").value = values;
-           
-        case '.':
-            document.getElementById("text").value = values;
-           
-        case '+':
-            document.getElementById("text").value = values;
-           
-        case '-':
-            document.getElementById("text").value = values;
-           
-        case 'x':
-            document.getElementById("text").value = values;
-           
-        case '÷':
-            document.getElementById("text").value = values;
-           
-        default:
-          
-      }*/
+function calculation(value) {
+    var currentText = document.getElementById("text").value;
+    document.getElementById("text").value = currentText + value;
 }
+
